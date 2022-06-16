4b732ce0a4c3d13ad186a3a6d8562943d031bc59
Matt Geale <matthewgeale@me.com>
2022-06-16 17:23:30 +1000

    jest config update

--- jest.config.js
+++ jest.config.js
@@ -1,22 +1,24 @@
-const { jestConfig } = require('@salesforce/sfdx-lwc-jest/config');
+const { jestConfig } = require("@salesforce/sfdx-lwc-jest/config");
 const setupFilesAfterEnv = jestConfig.setupFilesAfterEnv || [];
 module.exports = {
-    ...jestConfig,
-    moduleNameMapper: {
-        '^@salesforce/apex$': '<rootDir>/force-app/main/default/lwc/__tests__/jest-mocks/apex',
-        '^@salesforce/schema$': '<rootDir>/force-app/main/default/lwc/__tests__/jest-mocks/schema',
-        '^lightning/navigation$':
-            '<rootDir>/force-app/main/default/lwc/__tests__/jest-mocks/lightning/navigation',
-        '^lightning/platformShowToastEvent$':
-            '<rootDir>/force-app/main/default/lwc/__tests__/jest-mocks/lightning/platformShowToastEvent',
-        '^lightning/uiRecordApi$':
-            '<rootDir>/force-app/main/default/lwc/__tests__/jest-mocks/lightning/uiRecordApi',
-        '^lightning/messageService$':
-            '<rootDir>/force-app/main/default/lwc/__tests__/jest-mocks/lightning/messageService',
-        '^lightning/actions$':
-            '<rootDir>/force-app/main/default/lwc/__tests__/jest-mocks/lightning/actions'
-    },
-    setupFiles: ['jest-canvas-mock'],
-    setupFilesAfterEnv,
-    testTimeout: 10000
+  ...jestConfig,
+  moduleNameMapper: {
+    "^@salesforce/apex$":
+      "<rootDir>/force-app/main/default/lwc/__tests__/jest-mocks/apex",
+    "^@salesforce/schema$":
+      "<rootDir>/force-app/main/default/lwc/__tests__/jest-mocks/schema",
+    "^lightning/navigation$":
+      "<rootDir>/force-app/main/default/lwc/__tests__/jest-mocks/lightning/navigation",
+    "^lightning/platformShowToastEvent$":
+      "<rootDir>/force-app/main/default/lwc/__tests__/jest-mocks/lightning/platformShowToastEvent",
+    "^lightning/uiRecordApi$":
+      "<rootDir>/force-app/main/default/lwc/__tests__/jest-mocks/lightning/uiRecordApi",
+    "^lightning/messageService$":
+      "<rootDir>/force-app/main/default/lwc/__tests__/jest-mocks/lightning/messageService",
+    "^lightning/actions$":
+      "<rootDir>/force-app/main/default/lwc/__tests__/jest-mocks/lightning/actions"
+  },
+  setupFiles: ["jest-canvas-mock"],
+  setupFilesAfterEnv,
+  testTimeout: 10000
 };
