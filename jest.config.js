e24003bc3d28646846318a1889170f7aebc695eb
Matt Geale <matthewgeale@me.com>
2022-06-25 20:59:25 +1000

    moved lwc mocks

--- jest.config.js
+++ jest.config.js
@@ -3,20 +3,18 @@ const setupFilesAfterEnv = jestConfig.setupFilesAfterEnv || [];
 module.exports = {
   ...jestConfig,
   moduleNameMapper: {
-    "^@salesforce/apex$":
-      "<rootDir>/force-app/main/default/lwc/__tests__/jest-mocks/apex",
-    "^@salesforce/schema$":
-      "<rootDir>/force-app/main/default/lwc/__tests__/jest-mocks/schema",
+    "^@salesforce/apex$": "<rootDir>/core-app/tests/jest-mocks/apex",
+    "^@salesforce/schema$": "<rootDir>/core-app/tests/jest-mocks/schema",
     "^lightning/navigation$":
-      "<rootDir>/force-app/main/default/lwc/__tests__/jest-mocks/lightning/navigation",
+      "<rootDir>/core-app/tests/jest-mocks/lightning/navigation",
     "^lightning/platformShowToastEvent$":
-      "<rootDir>/force-app/main/default/lwc/__tests__/jest-mocks/lightning/platformShowToastEvent",
+      "<rootDir>/core-app/tests/jest-mocks/lightning/platformShowToastEvent",
     "^lightning/uiRecordApi$":
-      "<rootDir>/force-app/main/default/lwc/__tests__/jest-mocks/lightning/uiRecordApi",
+      "<rootDir>/core-app/tests/jest-mocks/lightning/uiRecordApi",
     "^lightning/messageService$":
-      "<rootDir>/force-app/main/default/lwc/__tests__/jest-mocks/lightning/messageService",
+      "<rootDir>/core-app/tests/jest-mocks/lightning/messageService",
     "^lightning/actions$":
-      "<rootDir>/force-app/main/default/lwc/__tests__/jest-mocks/lightning/actions"
+      "<rootDir>/core-app/tests/jest-mocks/lightning/actions"
   },
   setupFiles: ["jest-canvas-mock"],
   setupFilesAfterEnv,
